5afdefc2a898b83e94aa2418da0f840f47e5eb68
andytyc <andy.tengyc@gmail.com>
2023-03-02 19:58:08 +0800

    updated test

--- app/media/video/test/videoinfo/main.go
+++ app/media/video/test/videoinfo/main.go
@@ -24,24 +24,28 @@ func main() {
 
 		ret, videoFileInfo, err := video.GetMediaFileInfo(localFilename)
 		if err != nil {
+			log.Panicln(tagmsg, "err", err)
 			return
 		}
 		log.Println("file :", localFilename, "run command ret :", ret)
 
 		if videoFileInfo.Format.Size == "" {
 			err = fmt.Errorf("videoFileInfo.Format.Size is empty :%s", localFilename)
+			log.Panicln(tagmsg, "err", err)
 			return
 		}
 
 		fileSize, err := strconv.ParseInt(videoFileInfo.Format.Size, 10, 64)
 		if err != nil {
 			err = fmt.Errorf("videoFileInfo.Format.Size strconv.ParseInt failed :%s:%s", localFilename, videoFileInfo.Format.Size)
+			log.Panicln(tagmsg, "err", err)
 			return
 		}
 		log.Println(tagmsg, "文件大小", fileSize)
 
 		totalFrame, err := video.GetVideoTotalFrame(videoFileInfo)
 		if err != nil {
+			log.Panicln(tagmsg, "err", err)
 			return
 		}
 		log.Println(tagmsg, "文件总帧数", totalFrame)
