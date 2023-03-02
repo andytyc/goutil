c351894d0123db6a5a23fd03cfe6a52ce294a016
andytyc <andy.tengyc@gmail.com>
2023-03-02 19:49:13 +0800

    updated log

--- app/media/video/test/videoinfo/main.go
+++ app/media/video/test/videoinfo/main.go
@@ -30,14 +30,12 @@ func main() {
 
 		if videoFileInfo.Format.Size == "" {
 			err = fmt.Errorf("videoFileInfo.Format.Size is empty :%s", localFilename)
-			// logs.Error(tagmsg, "失败", err)
 			return
 		}
 
 		fileSize, err := strconv.ParseInt(videoFileInfo.Format.Size, 10, 64)
 		if err != nil {
 			err = fmt.Errorf("videoFileInfo.Format.Size strconv.ParseInt failed :%s:%s", localFilename, videoFileInfo.Format.Size)
-			// logs.Error(tagmsg, "失败", err)
 			return
 		}
 		log.Println(tagmsg, "文件大小", fileSize)
