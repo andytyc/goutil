6a8774a527f6be6cb0a7911af263ea452edb6dd2
andytyc <andy.tengyc@gmail.com>
2023-03-06 18:08:00 +0800

    add web

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # goutil
+
 helper for golang in running
+
+# 导航
+
+- [基础](./public/)
+
+- [应用](./app/)
+
+  - [多媒体](./app/media/)
+
+  - [web 服务]()
---
+++ app/web/httpdo/response.go
@@ -0,0 +1,36 @@
+package httpdo
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+)
+
+type ResData struct {
+	Code     int         `json:"code"`      // 自定义code
+	Message  string      `json:"message"`   // 提示信息
+	Data     interface{} `json:"data"`      // 返回数据
+	MoreInfo string      `json:"more_info"` // 一般返回失败时，捕捉的错误信息，用于开发者查看
+}
+
+func (r *ResData) Info() string {
+	return fmt.Sprintf("[%d] %s | %s", r.Code, r.Message, r.MoreInfo)
+}
+
+func UnmarshalBodyByJson(body io.ReadCloser, result *ResData) error {
+	if body == nil {
+		return fmt.Errorf("resp body is empty")
+	}
+	b, err := ioutil.ReadAll(body)
+	if err != nil {
+		err = fmt.Errorf("read resp body err %s", err)
+		return err
+	}
+	err = json.Unmarshal(b, result)
+	if err != nil {
+		err = fmt.Errorf("resp body json unmarshal err %s", err)
+		return err
+	}
+	return nil
+}
